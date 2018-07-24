64af4f91871e7ad4745e8e97a306a774ef136c9b
Adam Ratzman <adam@adamratzman.com>
2018-07-23 20:50:22 -0400

    allow generation of spotifyclientapis within SpotifyAPI

--- src/test/kotlin/com/adamratzman/spotify/main/SpotifyClientAPITest.kt
+++ src/test/kotlin/com/adamratzman/spotify/main/SpotifyClientAPITest.kt
@@ -5,6 +5,11 @@ import junit.framework.TestCase
 
 class SpotifyClientAPITest : TestCase() {
     fun testCreation() {
+       val api= SpotifyAPI.Builder("", "").build()
+        val client = api.authorizeUser("",
+                redirectUri = "https://ardentbot.com")
+        println(client.clientLibrary.getSavedTracks().complete().items)
+        // println(api.)
      //   Assert.assertArrayEquals(api.token!!.getScopes().toTypedArray(), arrayOf(SpotifyClientAPI.Scope.UGC_IMAGE_UPLOAD, SpotifyClientAPI.Scope.PLAYLIST_MODIFY_PRIVATE))
     }
 }
