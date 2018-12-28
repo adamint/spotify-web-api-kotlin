7e66ac7d19b862788a79a8e637fc5dc96f9b0a69
Adam Ratzman <adam@adamratzman.com>
2018-12-27 23:16:51 -0500

    condense ClientLibraryAPI methods (remove redundancy)

--- src/test/kotlin/com/adamratzman/spotify/private/ClientLibraryAPITest.kt
+++ src/test/kotlin/com/adamratzman/spotify/private/ClientLibraryAPITest.kt
@@ -1,7 +1,13 @@
 package com.adamratzman.spotify.private
 
+import com.adamratzman.spotify.api
+import com.adamratzman.spotify.main.SpotifyClientAPI
 import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 
 class ClientLibraryAPITest: Spek({
+    describe("Client Library tests") {
+        if (api !is SpotifyClientAPI) return@describe
 
+    }
 })
