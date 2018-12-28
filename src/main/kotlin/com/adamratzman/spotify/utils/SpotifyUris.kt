7e6cf665538967e97c0762b68b107e3ef00dc68f
Adam Ratzman <adam@adamratzman.com>
2018-12-28 13:47:39 -0500

    use SpotifyUri input field instead of parameter

--- src/main/kotlin/com/adamratzman/spotify/utils/SpotifyUris.kt
+++ src/main/kotlin/com/adamratzman/spotify/utils/SpotifyUris.kt
@@ -20,33 +20,33 @@ private fun String.remove(type: String): String {
     throw SpotifyUriException("Illegal Spotify ID/URI: '$this' isn't convertible to '$type' id")
 }
 
-abstract class SpotifyUri(tmp:String, val input: String = tmp.replace(" ", "")) {
-    abstract val uri:String
-    abstract val id:String
+abstract class SpotifyUri(tmp: String, val input: String = tmp.replace(" ", "")) {
+    abstract val uri: String
+    abstract val id: String
 }
 
-class AlbumURI(input: String):SpotifyUri(input) {
-    override val uri: String = input.add("album")
-    override val id: String = input.remove("album")
+class AlbumURI(input: String) : SpotifyUri(input) {
+    override val uri: String = super.input.add("album")
+    override val id: String = super.input.remove("album")
 }
 
-class ArtistURI(input: String):SpotifyUri(input) {
-    override val uri: String  = input.add("artist")
-    override val id: String = input.remove("artist")
+class ArtistURI(input: String) : SpotifyUri(input) {
+    override val uri: String = super.input.add("artist")
+    override val id: String = super.input.remove("artist")
 }
 
-class TrackURI(input: String):SpotifyUri(input) {
-    override val uri: String= input.add("track")
-    override val id: String= input.remove("track")
+class TrackURI(input: String) : SpotifyUri(input) {
+    override val uri: String = super.input.add("track")
+    override val id: String = super.input.remove("track")
 }
 
-class UserURI(input: String):SpotifyUri(input) {
-    override val uri: String = input.add("user")
-    override val id: String = input.remove("user")
+class UserURI(input: String) : SpotifyUri(input) {
+    override val uri: String = super.input.add("user")
+    override val id: String = super.input.remove("user")
 }
 
-class PlaylistURI(input: String) :SpotifyUri(input){
-    override val uri: String = input.add("playlist")
-    override val id: String= input.remove("playlist")
+class PlaylistURI(input: String) : SpotifyUri(input) {
+    override val uri: String = super.input.add("playlist")
+    override val id: String = super.input.remove("playlist")
 }
 
