086d008681a44533f310f2ea90fa5a72da6ca9a3
Adam Ratzman <adam@adamratzman.com>
2019-01-04 16:32:27 -0700

    remove linked results, document more objects
    Signed-off-by: Adam Ratzman <adam@adamratzman.com>

--- src/main/kotlin/com/adamratzman/spotify/utils/AuthObjects.kt
+++ src/main/kotlin/com/adamratzman/spotify/utils/AuthObjects.kt
@@ -12,15 +12,23 @@ data class Token(
     @Json(name = "token_type") val tokenType: String,
     @Json(name = "expires_in")val expiresIn: Int,
     @Json(name = "refresh_token") val refreshToken: String? = null,
-    @Json(ignored = false) private val scopeString: String? = null
-) {
+    @Json(ignored = false) private val scopeString: String? = null,
     @Json(ignored = true) val scopes: List<SpotifyScope>? = scopeString?.let { str ->
         str.split(" ").mapNotNull { scope -> SpotifyScope.values().find { it.uri == scope } }
     }
-}
+)
 
+/**
+ * Wrapper around [ErrorObject]
+ */
 data class ErrorResponse(val error: ErrorObject)
 
+/**
+ * Contains a parsed error from Spotify
+ *
+ * @property status The HTTP status code
+ * @property message A short description of the cause of the error.
+ */
 data class ErrorObject(val status: Int, val message: String)
 
 class SpotifyUriException(message: String) : BadRequestException(message)
