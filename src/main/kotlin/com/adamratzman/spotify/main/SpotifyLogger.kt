136699e72f373e545fa00c8125ccde30649ed7da
Adam Ratzman <adam@adamratzman.com>
2019-05-03 17:53:57 -0400

    retry on 502, don't log when logger is disabled

--- src/main/kotlin/com/adamratzman/spotify/main/SpotifyLogger.kt
+++ src/main/kotlin/com/adamratzman/spotify/main/SpotifyLogger.kt
@@ -4,10 +4,16 @@ package com.adamratzman.spotify.main
 class SpotifyLogger(var enabled: Boolean) {
     val redString = "\u001B[31m"
     val resetString = "\u001B[0m"
-    fun logInfo(message: String) = println("Spotify Logger Info: $message")
-    fun logWarning(message: String) = println("${redString}Spotify Logger Warning: $message$resetString")
+    fun logInfo(message: String) {
+        if (enabled) println("Spotify Logger Info: $message")
+    }
+    fun logWarning(message: String) {
+        if (enabled) println("${redString}Spotify Logger Warning: $message$resetString")
+    }
 
     fun logError(fatal: Boolean, message: String?, throwable: Throwable?) {
+        if (!enabled) return
+
         val sb = StringBuilder("${redString}Spotify Logger ")
         sb.append(if (fatal) "FATAL" else "Error")
         if (message != null) sb.append(": $message")
