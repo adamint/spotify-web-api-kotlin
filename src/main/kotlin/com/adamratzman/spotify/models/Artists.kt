3c16b47476abb23f82548bb52f0235103fad6acf
Adam Ratzman <adam@adamratzman.com>
2019-05-03 22:51:46 -0400

    refractor models by relationship, update docs

---
+++ src/main/kotlin/com/adamratzman/spotify/models/Artists.kt
@@ -0,0 +1,53 @@
+package com.adamratzman.spotify.models
+
+import com.beust.klaxon.Json
+
+/**
+ * @property externalUrls Known external URLs for this artist.
+ * @property href A link to the Web API endpoint providing full details of the artist.
+ * @property id The Spotify ID for the artist.
+ * @property name The name of the artist
+ * @property type The object type: "artist"
+ * @property uri The Spotify URI for the artist.
+ */
+data class SimpleArtist(
+        @Json(name = "external_urls") val externalUrls: Map<String, String>,
+        val href: String,
+        val id: String,
+        val name: String,
+        val type: String,
+        @Json(name = "uri", ignored = false) private val _uri: String,
+        @Json(ignored = true) val uri: ArtistURI = ArtistURI(_uri)
+) : Linkable() {
+    fun toFullArtist() = api.artists.getArtist(id)
+}
+
+/**
+ * @property externalUrls Known external URLs for this artist.
+ * @property followers Information about the followers of the artist.
+ * @property genres A list of the genres the artist is associated with. For example: "Prog Rock" ,
+ * "Post-Grunge". (If not yet classified, the array is empty.)
+ * @property href A link to the Web API endpoint providing full details of the artist.
+ * @property id The Spotify ID for the artist.
+ * @property images Images of the artist in various sizes, widest first.
+ * @property name The name of the artist
+ * @property popularity The popularity of the artist. The value will be between 0 and 100, with 100 being the most
+ * popular. The artist’s popularity is calculated from the popularity of all the artist’s tracks.
+ * @property type The object type: "artist"
+ * @property uri The Spotify URI for the artist.
+ */
+data class Artist(
+        @Json(name = "external_urls") val externalUrls: Map<String, String>,
+        val followers: Followers,
+        val genres: List<String>,
+        val href: String,
+        val id: String,
+        val images: List<SpotifyImage>,
+        val name: String,
+        val popularity: Int,
+        val type: String,
+        @Json(name = "uri", ignored = false) private val _uri: String,
+        @Json(ignored = true) val uri: ArtistURI = ArtistURI(_uri)
+)
+
+internal data class ArtistList(val artists: List<Artist?>)
---
+++ src/main/kotlin/com/adamratzman/spotify/models/Browse.kt
@@ -0,0 +1,51 @@
+package com.adamratzman.spotify.models
+
+/**
+ * Spotify music category
+ *
+ * @property href A link to the Web API endpoint returning full details of the category.
+ * @property icons The category icon, in various sizes.
+ * @property id The Spotify category ID of the category.
+ * @property name The name of the category.
+ */
+data class SpotifyCategory(
+        val href: String,
+        val icons: List<SpotifyImage>,
+        val id: String,
+        val name: String
+)
+
+/**
+ * Seed from which the recommendation was constructed
+ *
+ * @property initialPoolSize The number of recommended tracks available for this seed.
+ * @property afterFilteringSize The number of tracks available after min_* and max_* filters have been applied.
+ * @property afterRelinkingSize The number of tracks available after relinking for regional availability.
+ * @property href A link to the full track or artist data for this seed. For tracks this will be a link to a Track
+ * Object. For artists a link to an Artist Object. For genre seeds, this value will be null.
+ * @property id The id used to select this seed. This will be the same as the string used in the
+ * seed_artists , seed_tracks or seed_genres parameter.
+ * @property type The entity type of this seed. One of artist , track or genre.
+ */
+data class RecommendationSeed(
+        val initialPoolSize: Int,
+        val afterFilteringSize: Int,
+        val afterRelinkingSize: Int?,
+        val href: String?,
+        val id: String,
+        val type: String
+)
+
+/**
+ * @property seeds An array of recommendation seed objects.
+ * @property tracks An array of track object (simplified) ordered according to the parameters supplied.
+ */
+data class RecommendationResponse(val seeds: List<RecommendationSeed>, val tracks: List<SimpleTrack>)
+
+/**
+ * Spotify featured playlists (on the Browse tab)
+ *
+ * @property message the featured message in "Overview"
+ * @property playlists [PagingObject] of returned items
+ */
+data class FeaturedPlaylists(val message: String, val playlists: PagingObject<SimplePlaylist>)
