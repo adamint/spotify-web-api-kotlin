fc39746381c3b5494cd4366a13454338f260a37b
Adam Ratzman <adam@adamratzman.com>
2019-05-03 22:12:26 -0400

    add documentation for remaining player models

--- src/main/kotlin/com/adamratzman/spotify/utils/MiscUtils.kt
+++ src/main/kotlin/com/adamratzman/spotify/utils/MiscUtils.kt
@@ -2,6 +2,7 @@
 package com.adamratzman.spotify.utils
 
 import com.adamratzman.spotify.models.BadRequestException
+import com.adamratzman.spotify.models.ResultEnum
 
 internal fun <T> catch(function: () -> T): T? {
     return try {
@@ -10,3 +11,7 @@ internal fun <T> catch(function: () -> T): T? {
         null
     }
 }
+
+
+fun <T : ResultEnum> Array<T>.match(identifier: String)
+        = firstOrNull { it.retrieveIdentifier().toString().equals(identifier, true) }
