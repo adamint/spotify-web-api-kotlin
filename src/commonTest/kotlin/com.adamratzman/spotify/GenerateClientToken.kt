9980c8f293dd20959d5e88a0e002aef14604e57b
Adam Ratzman <adam@adamratzman.com>
2019-10-20 17:17:22 -0400

    add new explicit content field in user, utility for easy token gen

---
+++ src/commonTest/kotlin/com.adamratzman/spotify/GenerateClientToken.kt
@@ -0,0 +1,35 @@
+package com.adamratzman.spotify
+
+fun main(args: Array<String>) {
+    val _clientId = args[0]
+    val _clientSecret = args[1]
+    val _redirectUri = args[2]
+
+    val _authorizationCode = args.getOrNull(3)
+
+    if (args.size < 4) {
+        val authorizationUrl = spotifyClientApi {
+            credentials {
+                clientId = _clientId
+                clientSecret = _clientSecret
+                redirectUri = _redirectUri
+            }
+        }.getAuthorizationUrl(*SpotifyScope.values())
+
+        println(authorizationUrl)
+    } else {
+        val token = spotifyClientApi {
+            credentials {
+                clientId = _clientId
+                clientSecret = _clientSecret
+                redirectUri = _redirectUri
+            }
+            authorization {
+                authorizationCode = _authorizationCode!!
+            }
+        }.build().token
+
+        println(token)
+    }
+
+}
