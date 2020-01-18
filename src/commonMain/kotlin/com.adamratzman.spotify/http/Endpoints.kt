40e131830974013161664f32b0c67e941ad4cba0
Adam Ratzman <adam@adamratzman.com>
2020-01-18 18:40:55 -0500

    fix automatic token refresh for SpotifyClientApi, including with 401

--- src/commonMain/kotlin/com.adamratzman.spotify/http/Endpoints.kt
+++ src/commonMain/kotlin/com.adamratzman.spotify/http/Endpoints.kt
@@ -2,8 +2,9 @@
 package com.adamratzman.spotify.http
 
 import com.adamratzman.spotify.SpotifyApi
-import com.adamratzman.spotify.SpotifyAppApi
+import com.adamratzman.spotify.SpotifyClientApi
 import com.adamratzman.spotify.SpotifyException
+import com.adamratzman.spotify.SpotifyException.BadRequestException
 import com.adamratzman.spotify.SpotifyRestAction
 import com.adamratzman.spotify.SpotifyRestActionPaging
 import com.adamratzman.spotify.base
@@ -44,9 +45,10 @@ abstract class SpotifyEndpoint(val api: SpotifyApi<*, *>) {
         body: String? = null,
         method: HttpRequestMethod = HttpRequestMethod.GET,
         retry202: Boolean = true,
-        contentType: String? = null
+        contentType: String? = null,
+        attemptedRefresh: Boolean = false
     ): String {
-        if (api is SpotifyAppApi && getCurrentTimeMs() >= api.expireTime) {
+        if (api is SpotifyClientApi && getCurrentTimeMs() >= api.expireTime) {
             if (!api.automaticRefresh) throw SpotifyException.AuthenticationException("The access token has expired.")
             else api.refreshToken()
         }
@@ -59,19 +61,32 @@ abstract class SpotifyEndpoint(val api: SpotifyApi<*, *>) {
             cache -= spotifyRequest
         }
 
-        val document = createConnection(url, body, method, contentType).execute(
-            cacheState?.eTag?.let {
-                listOf(HttpHeader("If-None-Match", it))
-            }
-        )
+        return try {
+            val document = createConnection(url, body, method, contentType).execute(
+                    cacheState?.eTag?.let {
+                        listOf(HttpHeader("If-None-Match", it))
+                    }
+            )
 
-        return handleResponse(document, cacheState, spotifyRequest, retry202) ?: execute(
-            url,
-            body,
-            method,
-            false,
-            contentType
-        )
+            handleResponse(document, cacheState, spotifyRequest, retry202) ?: execute(
+                    url,
+                    body,
+                    method,
+                    false,
+                    contentType
+            )
+        } catch (e: BadRequestException) {
+            if (e.statusCode?.equals(401) == true && !attemptedRefresh) {
+                execute(
+                        url,
+                        body,
+                        method,
+                        retry202,
+                        contentType,
+                        true
+                )
+            } else throw e
+        }
     }
 
     private fun handleResponse(
