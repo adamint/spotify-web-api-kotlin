e4c1ca7b2b7940bfa62ec7d8e618515a8153adda
Adam Ratzman <adam@adamratzman.com>
2020-05-01 10:29:17 -0400

    replace base64 w/datatypeconverter in jvm

--- src/jvmMain/kotlin/com/adamratzman/spotify/utils/IO.kt
+++ src/jvmMain/kotlin/com/adamratzman/spotify/utils/IO.kt
@@ -4,12 +4,13 @@ package com.adamratzman.spotify.utils
 import java.io.ByteArrayOutputStream
 import java.net.URL
 import javax.imageio.ImageIO
+import javax.xml.bind.DatatypeConverter
 
 internal actual fun encodeBufferedImageToBase64String(image: BufferedImage): String {
     val bos = ByteArrayOutputStream()
     ImageIO.write(image, "jpg", bos)
     bos.close()
-    return javax.xml.bind.DatatypeConverter.printBase64Binary(bos.toByteArray())
+    return DatatypeConverter.printBase64Binary(bos.toByteArray())
 }
 
 internal actual fun convertFileToBufferedImage(file: File): BufferedImage = ImageIO.read(file)
