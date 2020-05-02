c15f938740cfe453d271900181a2468593b2752f
Adam Ratzman <adam@adamratzman.com>
2020-05-02 03:14:14 -0400

    fix getWithNext

--- src/commonTest/kotlin/com.adamratzman/spotify/utilities/RestActionTests.kt
+++ src/commonTest/kotlin/com.adamratzman/spotify/utilities/RestActionTests.kt
@@ -2,17 +2,25 @@
 package com.adamratzman.spotify.utilities
 
 import com.adamratzman.spotify.annotations.SpotifyExperimentalHttpApi
+import com.adamratzman.spotify.api
+import com.adamratzman.spotify.endpoints.public.SearchApi.SearchType.TRACK
+import com.adamratzman.spotify.utils.runBlocking
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
 
 @SpotifyExperimentalHttpApi
 class RestActionTests : Spek({
     describe("Paging Object") {
         it("next test") {
-            /*runBlocking {
+            runBlocking {
                 assertEquals(3, api.search.search("I", TRACK, limit = 10).complete().tracks!!.getWithNext(3).complete().toList().size)
                 assertEquals(45, api.search.searchTrack("I", limit = 15).getWithNextItems(3).complete().toList().size)
-            }*/
+            }
         }
+    }
+
+    describe("request timeout") {
+
     }
 })
