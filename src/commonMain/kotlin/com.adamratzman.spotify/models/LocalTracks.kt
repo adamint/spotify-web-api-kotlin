2c5464f8dd80ee58b6ddd89ba46ba287af66efea
Adam Ratzman <adam@adamratzman.com>
2020-05-05 16:26:23 -0400

    document local tracks

--- src/commonMain/kotlin/com.adamratzman.spotify/models/LocalTracks.kt
+++ src/commonMain/kotlin/com.adamratzman.spotify/models/LocalTracks.kt
@@ -37,6 +37,22 @@ data class SimpleLocalAlbum(
     val type: String
 )
 
+/**
+ * Local track object that representing a song uploaded from a client locally
+ *
+ * @property artists The artists who performed the track.
+ * @property discNumber The disc number.
+ * @property durationMs The track length in milliseconds.
+ * @property explicit Whether or not the track has explicit lyrics ( true = yes it does; false = no it does not OR unknown).
+ * @property href A link to the Web API endpoint providing full details of the track.
+ * @property id The Spotify ID for the track.
+ * @property name The name of the track.
+ * @property trackNumber The number of the track. If an album has several discs, the track number
+ * is the number on the specified disc.
+ * @property type The object type: “track”.
+ * @property isLocal Whether or not the track is from a local file.
+ * @property popularity the popularity of this track. possibly null
+ */
 @Serializable
 data class LocalTrack(
     val album: SimpleLocalAlbum,
