8f6c42144e6044dc8189e868b86811af964fcc63
Adam Ratzman <adam@adamratzman.com>
2020-08-03 17:14:56 -0400

    add test for pkce auth

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ buildscript {
 }
 
 group = "com.adamratzman"
-version = "3.2.0"
+version = "3.2.01"
 
 /*java {
     withSourcesJar()
@@ -165,6 +165,7 @@ kotlin {
                     implementation("org.spekframework.spek2:spek-dsl-jvm:$spekVersion")
                     runtimeOnly("org.spekframework.spek2:spek-runner-junit5:$spekVersion")
                     runtimeOnly(kotlin("reflect"))
+                    implementation("com.sparkjava:spark-core:2.9.1")
                 }
             }
 
