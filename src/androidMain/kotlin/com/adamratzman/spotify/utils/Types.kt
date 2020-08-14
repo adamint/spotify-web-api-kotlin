5a5c7927669c508e786892fe8bbdf7f76230673c
Adam Ratzman <adam@adamratzman.com>
2020-08-13 21:26:58 -0400

    implement images on android
    Signed-off-by: Adam Ratzman <adam@adamratzman.com>

--- src/androidMain/kotlin/com/adamratzman/spotify/utils/Types.kt
+++ src/androidMain/kotlin/com/adamratzman/spotify/utils/Types.kt
@@ -1,9 +1,11 @@
 /* Spotify Web API, Kotlin Wrapper; MIT License, 2017-2020; Original author: Adam Ratzman */
 package com.adamratzman.spotify.utils
 
+import android.graphics.Bitmap
+
 actual typealias ConcurrentHashMap<K, V> = java.util.concurrent.ConcurrentHashMap<K, V>
 
-actual typealias BufferedImage = String // TODO
+actual typealias BufferedImage = Bitmap // TODO
 
 actual typealias File = java.io.File
 
