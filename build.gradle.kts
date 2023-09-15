cda7135d7d2b5c2b900d233cb761bb8b5e0773cb
Ratzman, Adam Mortimer <aratzman@iu.edu>
2023-09-15 12:52:33 -0700

    update gradle/dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,9 +12,9 @@ plugins {
     signing
     id("com.android.library")
     kotlin("plugin.serialization")
-    id("com.diffplug.spotless") version "6.11.0"
+    id("com.diffplug.spotless") version "6.21.0"
     id("com.moowork.node") version "1.3.1"
-    id("org.jetbrains.dokka") version "1.8.20"
+    id("org.jetbrains.dokka") version "1.9.0"
 }
 
 repositories {
